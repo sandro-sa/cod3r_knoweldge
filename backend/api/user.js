d3c5a0a618f636d8ddeb7a92f5ad346cc3f92481
sandro-sa <sandrosa0315@outlook.com>
2023-06-15 22:14:36 -0300

    categoria

--- backend/api/user.js
+++ backend/api/user.js
@@ -12,9 +12,7 @@ module.exports = app => {
         const user = { ...req.body }
         if(req.params.id) user.id = req.params.id
 
-        if(!req.originalUrl.startsWith('/users')) user.admin = false
-        if(!req.user || !req.user.admin) user.admin = false
-
+    
         try {
             existsOrError(user.name, 'Nome não informado')
             existsOrError(user.email, 'E-mail não informado')
@@ -39,7 +37,6 @@ module.exports = app => {
             app.db('users')
                 .update(user)
                 .where({ id: user.id })
-                .whereNull('deletedAt')
                 .then(_ => res.status(204).send())
                 .catch(err => res.status(500).send(err))
         } else {
@@ -53,10 +50,18 @@ module.exports = app => {
     const get = (req, res) => {
         app.db('users')
             .select('id', 'name', 'email', 'admin')
-            .whereNull('deletedAt')
             .then(users => res.json(users))
             .catch(err => res.status(500).send(err))
     }
 
-  return { save, get }
+    const getById = (req, res) => {
+        app.db('users')
+            .select('name', 'email', 'admin')
+            .where({ id: req.params.id })
+            .first()
+            .then(users => res.json(users))
+            .catch(err => res.status(500).send(err))
+    }
+
+  return { save, get, getById }
 }
